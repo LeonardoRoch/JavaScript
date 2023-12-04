37cb631eb8042cd926a21925405dd1c7df4325c4
LeonardoRoch <151729722+LeonardoRoch@users.noreply.github.com>
2023-12-03 21:12:06 -0300

    Add files via upload

---
+++ Exercícios/001/script.js
@@ -0,0 +1,10 @@
+document.write("Olá, Mundo <br>") 
+
+var mensagem = "Olá, Mundo com var"
+
+let mensagem2 = "Olá, Mundo com let"
+
+const mensagem3 = "Olá, mundo com const"
+
+document.write(mensagem)
+document.write(mensagem2)
